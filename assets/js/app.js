5c678b3cfe49c7905edb7c842e1d27a0f87d5b52
Gordias <gordias.farhad@gmail.com>
2018-10-27 00:26:51 -0700

    update page

---
+++ assets/js/app.js
@@ -0,0 +1,48 @@
+var  nav = document.getElementById('nav');
+	window.onscroll = function () {
+		if (window.pageYOffset > 600){
+			nav.style.background = "grey";
+		}
+		else{
+			nav.style.background = "transparent";
+		}
+	}
+
+
+var i = 1;
+var j = 1;
+    function typer(){
+        var name = "FARHAD OMIDVAR";
+        if( i <= name.length ){ 
+            var txt = name.substring(0,i);
+            document.getElementById("name").innerHTML = txt;
+            setTimeout("typer()",200);
+            i++;
+        }
+        else{ 
+            i = 1;
+            document.getElementById("name").innerHTML = name;
+        }
+      
+    }
+
+    function typer1(){
+        var title = "Data Sceintist";
+        if( j <= title.length ){ 
+            var txt2 = title.substring(0,j);
+            document.getElementById("title").innerHTML = txt2;
+            setTimeout("typer1()",200);
+            j++;
+        }
+        else{ 
+            j = 1;
+            document.getElementById("title").innerHTML = title;
+        }
+    }
+
+    
+    
+window.onload = typer();
+window.onload = typer1();
+
+    
